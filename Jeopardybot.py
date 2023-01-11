95867d0be6b1a839f13f6cd111b048ce180323fa
Danras4300 <101792807+Danras4300@users.noreply.github.com>
2023-01-11 10:39:02 +0100

    big edit
    working sender

--- Jeopardybot.py
+++ Jeopardybot.py
@@ -1,5 +1,7 @@
 from pdb import Restart
 import discord
+import Answers as a
+import Questions as q
 import class_object as co
 import Images.Landmark as l
 import Images.Celeb as c
@@ -12,6 +14,8 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 TOK_FILE = "token.txt"
+
+Players = {}
 
 def get_token():
   tokfile = open(TOK_FILE, 'r')
@@ -26,11 +30,36 @@ async def on_ready():
 @client.event
 async def on_message(message):
   user_id = message.author.id
-  info = message.content
-  if info == "join":
-    
-  print(info)
+  contents = message.content
+  if contents.startswith("!join"):
+    globals()[user_id] = []
+    Players.update({str(user_id): globals()[user_id]})
+    #reply = ["you have now join the game", 
+    #         co.get_grid_lines(co.grid)]
+    reply = co.get_grid_lines(co.grid)
+    await message.channel.send("\n".join(reply))
+  elif str(user_id) in Players:
+    if contents.startswith("!math"):
+      points = int(contents[6:])
+      if points in q.Math_q:
+        reply = q.Math_q[points]
+        await message.channel.send(reply)
+      else:
+        reply = points, "is not possible to pick"
+        await message.channel.send(reply)
+    elif contents.startswith("!Capital"):
+      points = int(contents[9:])
+      if points in q.Capital_q:
+        reply = q.Capital_q[points]
+        await message.channel.send(reply)
+      else:
+        reply = points, "is not possible to pick"
+        await message.channel.send(reply)
+
+
+  print(contents)
   print(user_id)
+  print(Players)
 
 token = get_token()
 client.run(token)
--- class_object.py
+++ class_object.py
@@ -14,3 +14,43 @@ capital = Catagory(q.Capital_q, a.Capital_a)
 celeb = Catagory(q.Celeb_q, a.Celeb_a)
 Astronomy_q = Catagory(q.Astronomy_q, a.Astronomy_a)
 landmark = Catagory(q.Landmark_q, a.Landmark_a)
+
+grid = [["Math", "Capital", "Celeb", "Astronomy", "Landmark"],
+        [100, 100, 100, 100, 100],
+        [200, 200, 200, 200, 200],
+        [300, 300, 300, 300, 300],
+        [400, 400, 400, 400, 400],
+        [500, 500, 500, 500, 500],]
+
+def get_grid_lines(grid):
+    grid_lines = []
+    for row in grid:
+        row_str = ''
+        for value in row:
+            if value == "Math":
+                row_str += 'Math   '
+            elif value == "Capital":
+                row_str += 'Capital   '
+            elif value == "Celeb":
+                row_str += 'Celeb  '
+            elif value == "Astronomy":
+                row_str += 'Astronomy '
+            elif value == "Landmark":
+                row_str += 'Landmark'
+            elif value == 100:
+                row_str += '100      '
+            elif value == 200:
+                row_str += '200      '
+            elif value == 300:
+                row_str += '300      '
+            elif value == 400:
+                row_str += '400      '
+            elif value == 500:
+                row_str += '500      '
+            else:
+                row_str += 'XXX'
+        grid_lines.append(row_str)
+    print(grid_lines)
+    return grid_lines
+
+print("\n".join(get_grid_lines(grid)))
